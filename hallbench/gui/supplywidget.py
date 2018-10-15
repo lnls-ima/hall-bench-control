462da375e4415f64e02b69298510d9f581c4df86
VitorPS <vitorps@protonmail.com>
2018-10-15 12:18:18 -0300

    Typos corrections.

--- hallbench/gui/supplywidget.py
+++ hallbench/gui/supplywidget.py
@@ -1055,8 +1055,8 @@ class SupplyWidget(_QWidget):
                 f = float(self.ui.le_sinusoidal_frequency.text())
                 ncycles = int(self.ui.le_sinusoidal_ncycles.text())
                 theta = float(self.ui.le_sinusoidal_phase.text())
-                if any(a == 0, f == 0, ncycles == 0):
-                    _QMessageBox.waring(self, 'Warning',
+                if any([a == 0, f == 0, ncycles == 0]):
+                    _QMessageBox.warning(self, 'Warning',
                                         'Please check the parameters.',
                                         _QMessageBox.Ok)
                     return
@@ -1071,8 +1071,8 @@ class SupplyWidget(_QWidget):
                 ncycles = int(self.ui.le_damp_sin_ncycles.text())
                 theta = float(self.ui.le_damp_sin_phase.text())
                 tau = float(self.ui.le_damp_sin_damping.text())
-                if any(a == 0, f == 0, ncycles == 0, tau == 0):
-                    _QMessageBox.waring(self, 'Warning',
+                if any([a == 0, f == 0, ncycles == 0, tau == 0]):
+                    _QMessageBox.warning(self, 'Warning',
                                         'Please check the parameters.',
                                         _QMessageBox.Ok)
                     return
